98709e36088ee828a7d035bbeb5cab8cb4f62da5
Alex Gaignard <alex.gaignard@epitech.eu>
2022-02-28 15:24:44 -0800

    Fixed incorrect length

--- studentsolver.py
+++ studentsolver.py
@@ -26,39 +26,25 @@ def find_m_max(bulbs):
 
 def create_sets(bulbs, min, max):
     sets = {}
-    for i in range(min, max):
+    for i in range(min, max + 1):
         sets[i] = []
     for i in range(len(bulbs)):
         for j in bulbs[i]:
             idx = j
             if (idx < 0):
                 idx = idx * -1
-            if idx in sets:
-                if (j > 0):
-                    sets[idx].append({
-                        'is_red': False,
-                        'row_idx': i,
-                        'is_row_lit': False
-                    })
-                else:
-                    sets[idx].append({
-                        'is_red': True,
-                        'row_idx': i,
-                        'is_row_lit': False
-                    })
+            if (j > 0):
+                sets[idx].append({
+                    'is_red': False,
+                    'row_idx': i,
+                    'is_row_lit': False
+                })
             else:
-                if (j > 0):
-                    sets[idx] = [{
-                        'is_red': False,
-                        'row_idx': i,
-                        'is_row_lit': False
-                    }]
-                else:
-                    sets[idx] = [{
-                        'is_red': True,
-                        'row_idx': i,
-                        'is_row_lit': False
-                    }]
+                sets[idx].append({
+                    'is_red': True,
+                    'row_idx': i,
+                    'is_row_lit': False
+                })
     od = collections.OrderedDict(sorted(sets.items()))           
     return od
 
@@ -88,15 +74,15 @@ def lit_them_up(sets):
         if (weight < 0):
             result.append(False)
             for item in v:
-                if item['is_red']:
+                if item['is_red'] and not item['is_row_lit']:
                     sets = lit_row(sets, item['row_idx'])
         else:
             result.append(True)
             for item in v:
-                if not item['is_red']:
+                if not item['is_red'] and not item['is_row_lit']:
                     sets = lit_row(sets, item['row_idx'])
-    # print_sets(sets)
-    # print(result)
+    print_sets(sets)
+    print(result)
     return result
 
 def solve(bulbs):
@@ -108,8 +94,9 @@ def solve(bulbs):
 
     return lit_them_up(sets)
 
-# def main():
-#     bulbs = [[-4, -4, -1], [13, -9, -14], [4, -7, -2], [-2, 6, 3], [-6, -8, 11], [-4, -3 -6], [7, 11, -10], [4, -7, 6], [5, 6, 4], [10, 14, -9]]
-#     solve(bulbs)
+def main():
+    bulbs = [[1, -2, 2], [1, 1, 1], [-5, -2, 1], [4, -3, 3]]
+    # bulbs = [[-4, -4, -1], [13, -9, -14], [4, -7, -2], [-2, 6, 3], [-6, -8, 11], [-4, -3 -6], [7, 11, -10], [4, -7, 6], [5, 6, 4], [10, 14, -9]]
+    solve(bulbs)
 
-# main()
+main()
